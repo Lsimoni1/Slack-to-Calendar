4f31079c26dd2169c9ea60ac872b6512abe168c7
Lsimoni1 <luke.simoni2001@gmail.com>
2026-03-13 16:11:05 -0700

    fetch and download new schedule files

---
+++ slack_client.py
@@ -0,0 +1,52 @@
+import config
+from slack_sdk import WebClient
+import requests
+
+slack_token = config.SLACK_BOT_TOKEN
+slack_channel = config.SLACK_CHANNEL_ID
+
+def fetch_new_schedule():
+    client = WebClient(token = slack_token)
+    messages = client.conversations_history(channel = slack_channel)
+    files = []
+
+    try:
+        with open("latest_processed_file", "x") as f:
+            f.write("0")
+    except:
+        pass
+
+    with open("latest_processed_file") as f:
+        latest_ts = f.read()
+
+    for message in messages["messages"]:
+        if "files" in message: 
+            if message["files"][0]["ts"] > latest_ts:
+                files.append(message)
+
+    if not files:
+        return None
+    
+    with open("latest_processed_file", "w") as f:
+        f.write(files[-1]["ts"])
+    return files
+
+def download_schedules():
+    files = fetch_new_schedule() 
+    if not files:
+        return
+    
+    url = files[-1]["files"][0]["url_private"]
+    headers = {
+        "Authorization" : "Bearer " + slack_token
+    }
+    response = requests.get(url, headers = headers)
+
+    with open("latest_schedule", "wb") as f:
+        f.write(response.content)
+
+    return "latest_schedule"
+    
+
+    
+        
